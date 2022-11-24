9d46a7ef64384e061fc42575ac02d35541a4d68d
Jacob Simmons <jakerstx@gmail.com>
2022-11-24 00:43:23 -0600

    adds javscript functions to add, subtract, multiply and divide numbers.

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const add = function(a, b) {
+	return a + b
+};
+
+const subtract = function(a, b) {
+  return a - b
+};
+
+const sum = function(args) {
+  return args.reduce((acc, curr) => (acc + curr), 0)
+};
+
+const multiply = function(a) {
+  return a.reduce((acc, curr) => (acc * curr), 1)
+};
+
 const seven = document.querySelector('.seven')
 seven.addEventListener('click', () => {
     alert("works")
